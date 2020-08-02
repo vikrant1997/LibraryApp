9abd7249ceffbc8cf69aea05276e2e1f58e4b148
SINGH VIKRANT <vikrant@geekyants.com>
2020-08-02 16:46:28 +0530

    author controller refactored

--- src/Controllers/authController.ts
+++ src/Controllers/authController.ts
@@ -1,48 +1,45 @@
-// import Author from '../models'
-import express, {
-  Request as Req,
-  Response as Res,
-  NextFunction as Next,
-} from 'express'
+import HttpException from '../exceptions/HttpException'
+import { Logger } from '../middleware'
+import { IRequest, IResponse, INext } from '../interfaces/vendors'
+import { Author } from '../models'
 
-const authorList = (req: Req, res: Res) => {
-  // Author.find()
-  //   .populate('author')
-  //   .sort([['familyName ', 'ascending']])
-  //   .exec(function (err, listAuthors) {
-  //     if (err) {
-  //       return next(err)
-  //     }
-  //     res.send(listAuthors)
-  //   })
-  res.send('Not Implemented: Author List GET')
+const authorList = (req: IRequest, res: IResponse, next: INext): void => {
+  Author.find()
+    .populate('author')
+    .sort([['familyName ', 'ascending']])
+    .exec((err: Error, listAuthors) => {
+      if (err) {
+        return next(err)
+      }
+      res.send(listAuthors)
+    })
 }
 
-const authorDetail = (req: Req, res: Res) => {
+const authorDetail = (req: IRequest, res: IResponse) => {
   res.send(`Not Implemented: Author Detail ${req.params.id} ${req.params.id}`)
 }
 
-const authorCreateGet = (req: Req, res: Res) => {
+const authorCreateGet = (req: IRequest, res: IResponse) => {
   res.send('Not Implemented: Author create GET')
 }
 
-const authorCreatePost = (req: Req, res: Res) => {
+const authorCreatePost = (req: IRequest, res: IResponse) => {
   res.send('Not Implemented: Author create POST')
 }
 
-const authorDeleteGet = (req: Req, res: Res) => {
+const authorDeleteGet = (req: IRequest, res: IResponse) => {
   res.send('Not Implemented: Author delete GET')
 }
 
-const authoreDeltePost = (req: Req, res: Res) => {
+const authoreDeltePost = (req: IRequest, res: IResponse) => {
   res.send('Not Implemented: Author delte POST')
 }
 
-const authorUpdateGet = (req: Req, res: Res) => {
+const authorUpdateGet = (req: IRequest, res: IResponse) => {
   res.send('Not Implemented: Author update GET')
 }
 
-const authorUpdatePost = (req: Req, res: Res) => {
+const authorUpdatePost = (req: IRequest, res: IResponse) => {
   res.send('Not Implemented:Author update POST')
 }
 
