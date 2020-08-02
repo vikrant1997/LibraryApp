42f5f3e388fc26ec63824957b66178872de10927
SINGH VIKRANT <vikrant@geekyants.com>
2020-08-02 16:46:12 +0530

    home page controller refactored

--- src/Controllers/homePageController.ts
+++ src/Controllers/homePageController.ts
@@ -1,39 +1,34 @@
 import async from 'async'
-import express, {
-  Request as Req,
-  Response as Res,
-  NextFunction as Next,
-} from 'express'
-// const { Book, Author, BookInstance, Genre } = require('../models')
+import { IRequest, IResponse } from '../interfaces/vendors'
+import { Book, BookInstance, Author, Genre } from '../models'
 
-const index = function (req: Req, res: Res) {
-  // async.parallel(
-  //   {
-  //     bookCount(callback) {
-  //       Book.countDocuments({}, callback)
-  //     },
-  //     bookInstance(callback) {
-  //       BookInstance.countDocuments({}, callback)
-  //     },
-  //     bookInstancAvailableCount(callback) {
-  //       BookInstance.countDocuments({ status: 'Available' }, callback)
-  //     },
-  //     authorCount(callback) {
-  //       Author.countDocuments({}, callback)
-  //     },
-  //     genreCount(callback) {
-  //       Genre.countDocuments({}, callback)
-  //     },
-  //   },
+const index = (req: IRequest, res: IResponse) => {
+  async.parallel(
+    {
+      bookCount(callback) {
+        Book.countDocuments({}, callback)
+      },
+      bookInstance(callback) {
+        BookInstance.countDocuments({}, callback)
+      },
+      bookInstancAvailableCount(callback) {
+        BookInstance.countDocuments({ status: 'Available' }, callback)
+      },
+      authorCount(callback) {
+        Author.countDocuments({}, callback)
+      },
+      genreCount(callback) {
+        Genre.countDocuments({}, callback)
+      },
+    },
 
-  //   function (err, results) {
-  //     console.log(err)
-  //     if (err) throw err
-  //     console.log(results)
-  //     res.send(results)
-  //   },
-  // )
-  res.send('home Page ')
+    (err, results) => {
+      // console.log(err)
+      if (err) throw err
+      // console.log(results)
+      res.send(results)
+    },
+  )
 }
 
 export { index }
