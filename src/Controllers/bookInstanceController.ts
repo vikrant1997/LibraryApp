6ae4be4b9d1864ae93f9a6c88d2f8e109257b67b
SINGH VIKRANT <vikrant@geekyants.com>
2020-08-02 16:46:44 +0530

    book Instance controller added

--- src/Controllers/bookInstanceController.ts
+++ src/Controllers/bookInstanceController.ts
@@ -1,47 +1,43 @@
-// const { BookInstance } = require('../models')
-import express, {
-  Request as Req,
-  Response as Res,
-  NextFunction as Next,
-} from 'express'
+import { IRequest, IResponse, INext } from '../interfaces/vendors'
+import { BookInstance } from '../models'
 
-const bookInstanceList = (req: Req, res: Res) => {
-  // BookInstance.find()
-  //   .populate('book')
-  //   .exec(function (err, listInsatances) {
-  //     if (err) {
-  //       return next(err)
-  //     }
-  //     res.send(listInsatances)
-  //   })
-  res.send('NOT IMPLEMENTED: BookInstance list')
+const bookInstanceList = (req: IRequest, res: IResponse, next: INext) => {
+  BookInstance.find()
+    .populate('book')
+    .exec((err, listInsatances) => {
+      if (err) {
+        return next(err)
+      }
+      res.send(listInsatances)
+    })
+  // res.send('NOT IMPLEMENTED: BookInstance list')
 }
 
-const bookInstanceDetail = (req: Req, res: Res) => {
+const bookInstanceDetail = (req: IRequest, res: IResponse) => {
   res.send(`NOT IMPLEMENTED: BookInstance detail: ${req.params.id}`)
 }
 
-const bookInstanceCreateGet = (req: Req, res: Res) => {
+const bookInstanceCreateGet = (req: IRequest, res: IResponse) => {
   res.send('NOT IMPLEMENTED: BookInstance create GET')
 }
 
-const bookInstanceCreatePost = (req: Req, res: Res) => {
+const bookInstanceCreatePost = (req: IRequest, res: IResponse) => {
   res.send('NOT IMPLEMENTED: BookInstance create POST')
 }
 
-const bookInstanceDeleteGet = (req: Req, res: Res) => {
+const bookInstanceDeleteGet = (req: IRequest, res: IResponse) => {
   res.send('NOT IMPLEMENTED: BookInstance delete GET')
 }
 
-const bookInstanceDeletePost = (req: Req, res: Res) => {
+const bookInstanceDeletePost = (req: IRequest, res: IResponse) => {
   res.send('NOT IMPLEMENTED: BookInstance delete POST')
 }
 
-const bookInstanceUpdateGet = (req: Req, res: Res) => {
+const bookInstanceUpdateGet = (req: IRequest, res: IResponse) => {
   res.send('NOT IMPLEMENTED: BookInstance update GET')
 }
 
-const bookInstanceUpdatePost = (req: Req, res: Res) => {
+const bookInstanceUpdatePost = (req: IRequest, res: IResponse) => {
   res.send('NOT IMPLEMENTED: BookInstance update POST')
 }
 export {
