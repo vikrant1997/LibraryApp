c270f543632c9bf209df0e9f915c22e4375e4163
SINGH VIKRANT <vikrant@geekyants.com>
2020-08-03 11:16:15 +0530

    logger used in app

--- dist/app.js
+++ dist/app.js
@@ -6,29 +6,38 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const path_1 = __importDefault(require("path"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const morgan_1 = __importDefault(require("morgan"));
-const mongoose_1 = __importDefault(require("mongoose"));
 const express_1 = __importDefault(require("express"));
+const winston_1 = __importDefault(require("winston"));
 const routes_1 = __importDefault(require("./routes"));
+const providers_1 = require("./providers");
 const middleware_1 = require("./middleware");
+providers_1.Database.init();
 const app = express_1.default();
-app.use(morgan_1.default('dev'));
-// Set up default mongoose connection
-const mongoDB = 'mongodb://127.0.0.1/libraryDB';
-mongoose_1.default
-    .connect(mongoDB, { useUnifiedTopology: true, useNewUrlParser: true })
-    .catch((err) => middleware_1.Logger.warn(err));
-// Get the default connection
-const db = mongoose_1.default.connection;
-// Bind connection to error event (to get notification of connection errors)
-db.on('error', (err) => middleware_1.Logger.warn(err));
-db.once('open', () => {
-    middleware_1.Logger.info(`We are connected to Mongoose at ${mongoDB}!!!`);
-});
-app.use(morgan_1.default('dev'));
+// const wlogger = winston
+app.use(morgan_1.default('short', { stream: winston_1.default.stream }));
+// app.use(
+//   expressWinston.logger({
+//     transports: [new winston.transports.Console()],
+//     format: winston.format.combine(
+//       winston.format.colorize(),
+//       winston.format.json(),
+//     ),
+//     responseWhitelist: ['body'],
+//     meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+//     msg:
+//       'HTTP {{req.method}} {{req.url}} {{res.statusCode}} {{res.responseTime}}ms', // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+//     expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+//     colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+//     ignoreRoute(req, res) {
+//       return false
+//     }, // optional: allows to skip some log messages based on request and/or response
+//   }),
+// )
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use(cookie_parser_1.default());
 app.use(express_1.default.static(path_1.default.join(__dirname, 'public')));
 app.use(routes_1.default);
+app.use(morgan_1.default(':body'));
 app.use(middleware_1.errorMiddleware);
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,27 @@
 import path from 'path'
 import cookieParser from 'cookie-parser'
-import logger from 'morgan'
-import mongoose from 'mongoose'
-
+import morgan from 'morgan'
 import express from 'express'
 
 import router from './routes'
-
+import { Database } from './providers'
 import { errorMiddleware, Logger } from './middleware'
 
+Database.init()
+
 const app = express()
-app.use(logger('dev'))
+const winston = new Logger('app')
 
-// Set up default mongoose connection
-const mongoDB = 'mongodb://127.0.0.1/libraryDB'
-mongoose
-  .connect(mongoDB, { useUnifiedTopology: true, useNewUrlParser: true })
-  .catch((err) => Logger.warn(err))
+app.use(morgan('short', { stream: winston.stream }))
 
-// Get the default connection
-const db = mongoose.connection
-
-// Bind connection to error event (to get notification of connection errors)
-db.on('error', (err) => Logger.warn(err))
-db.once('open', () => {
-  Logger.info(`We are connected to Mongoose at ${mongoDB}!!!`)
-})
-
-app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(router)
+
+app.use(morgan(':body'))
 
 app.use(errorMiddleware)
 
